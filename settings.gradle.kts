b0438e08429874b392d6276dd87f16e9886f5605
IssaC114 <yongxi696@gmail.com>
2024-05-16 16:58:25 +0800

    test

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Scenic Spot to Map"
+include(":app")
